93f4a43a81e9fa44a4c043f8994ad985f450d2f5
Sachin Kumar <sachinstudent12345@gmail.com>
2026-09-18 17:43:30 +0530

    Fix dashboard path

--- script.js
+++ script.js
@@ -108,7 +108,7 @@ async function demoLogin() {
                 JSON.stringify(data.student)
             );
 
-          window.location.href = "/sgi-smart-campus-360/dashboard.html";
+       window.location.href = "/sgi-smart-campus-360/frontend/dashboard.html";
 
         } else {
 
